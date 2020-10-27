71579b4bff1dbdfd2ecf427235f1c9c9a975bdb2
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-10-27 20:47:11 +0900

    モデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birth_day, presence: true
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]\z/} do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
 end
