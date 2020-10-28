3030d3a68d7e221726c965ea3590922ff2cb33df
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-10-28 16:38:06 +0900

    テストコードの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name             {"かなカナ漢字"}
+    first_name            {"かなカナ漢字"}
+    last_name_kana        {"カタカナ"}
+    first_name_kana       {"カタカナ"}
+    birth_day             {"2020-01-01"}
+  end
+end
