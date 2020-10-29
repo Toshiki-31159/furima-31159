756e0360b8f7ed945d935bed7f41d27d0455045d
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-10-29 12:16:49 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :sipping_day_id
+    validates :price, format: {with: /\A[0-9]+\z/}
+  end
 end
