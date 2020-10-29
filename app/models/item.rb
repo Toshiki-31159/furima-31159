15fe182cc9662b8eeb27714fa74657bd398b9f7d
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-10-29 14:43:22 +0900

    アクティブハッシュの配列設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :item_status, :delivery_charge,
+                         :prefecture, :sipping_day
 
   with_options presence: true do
-    validates :name
-    validates :description
-    validates :category_id
-    validates :item_status_id
-    validates :delivery_charge_id
-    validates :prefecture_id
-    validates :sipping_day_id
+    validates :name, :description
     validates :price, format: {with: /\A[0-9]+\z/}
+  end
+  with_options presence: true, numericality: {other_than: 1} do
+    validates :category_id, :item_status_id, :delivery_charge_id,
+              :prefecture_id, :sipping_day_id
   end
 end
