7bfbc97c9372015f7ab8cce4fe2f387f4dbdcb3d
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-11-03 18:16:08 +0900

    テーブルの作成

--- db/migrate/20201103090112_create_street_addresses.rb
+++ db/migrate/20201103090112_create_street_addresses.rb
@@ -2,6 +2,13 @@ class CreateStreetAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :street_addresses do |t|
 
+      t.string     :postal_code,    nill: false
+      t.string     :phone_number,   nill: false
+      t.integer    :prefecture_id,  nill: false
+      t.string     :municipality,   nill: false
+      t.string     :address,        nill: false
+      t.string     :building_name
+      t.references :purchase_record, foreign_key: true
       t.timestamps
     end
   end
