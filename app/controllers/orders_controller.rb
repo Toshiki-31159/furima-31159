1b8d986475b4429548965ae17b60830a74ec01a3
Toshiki-31159 <t.toshiki.101@users.noreply.github.com>
2020-11-06 09:47:46 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,39 @@
 class OrdersController < ApplicationController
+before_action :item_params
+
   def index
     @order = ItemOrder.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @order = ItemOrder.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
-      binding.pry
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
 
   private
+
   def order_params
-    params.require(:item_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_order).permit(
+      :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(
+        user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def item_params
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -1,12 +1,12 @@
 class ItemOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :phone_number, format: { with: /\A\d{11}\z/ }
     validates :prefecture_id, numericality: {other_than: 1}
-    validates :municipality, :address
+    validates :municipality, :address, :token
   end
   validate :building_name
 
